8634a88b0e251a3643a1c74663d1897eac6cac41
admin <vanessa.soto@tsoftlatam.com>
2020-01-07 10:51:42 -0300

    Corrección junit

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -10,6 +10,8 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
+      require('karma-junit-reporter'),
+      require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
